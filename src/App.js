86b6504dd1380df53a15813cbfc94985302cf56a
Hans Naert <hans.naert@vives.be>
2022-05-12 22:15:44 +0200

    react-router - getting started guide

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Button variant="contained">Hello World</Button>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
